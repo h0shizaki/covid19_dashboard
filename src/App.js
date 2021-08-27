701f28a8864ecdc670aa6aa89187a806ef21a347
Kitsune-fx <kant.cmd@gmail.com>
2021-08-27 21:30:17 +0700

    :zap:first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const API_url = 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-all';
+
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    axios.get(API_url).then((result) => {
+      console.log(result.data);
+      setState(result.data);
+    })
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div>
+        {state && state.map(item =>
+          <div className='container'>
+            <div className='text'>Covid-19 Today </div>
+            <div className='date'>{item.txn_date}</div>
+            <div className='containerDetail'>
+              <div className='todayCase'> Today case: {item.new_case} </div>
+              <div className='totalCase'> Total case: {item.total_case} </div>
+            </div>
+            <div className='updateDate'> Update at : {item.update_date} </div>
+          </div>
+        )}
+      </div>
+    </center>
   );
 }
 
